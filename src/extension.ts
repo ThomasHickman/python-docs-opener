f209517a7a1c5681a095891b0e47dde87eb8d20e
Thomas Hickman <Thomas.Hickman42@gmail.com>
2021-10-09 12:36:35 +0100

    Make the endpoint get triggered on a keyboard shortcut

--- src/extension.ts
+++ src/extension.ts
@@ -5,6 +5,7 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	const logs = vscode.window.createOutputChannel("python-help-fetcher logs");
 	
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
@@ -13,11 +14,22 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('python-help-fetcher.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Thomas!');
+	let disposable = vscode.commands.registerCommand('python-help-fetcher.getDocsOfSymbol', () => {
+		const editor  = vscode.window.activeTextEditor;
+
+		logs.appendLine("Command triggered")
+		logs.show();
+
+		if (editor) {
+			const document = editor.document;
+			const selection = editor.selection;
+
+			const wordSelected = document.getText(selection);
+			vscode.window.showInformationMessage(`Word selected: ${wordSelected} at line ${selection.active.line}`);
+		}
+		
 	});
+
 
 	context.subscriptions.push(disposable);
 }
